4693caa1b373d8f3d902d3b453fdd2c568b848f1
ZAPHODh <102484511+ZAPHODh@users.noreply.github.com>
2023-01-22 09:43:52 -0300

    Fixed issue in the custom hook useScreenWidth

--- src/customHooks/useScreenWidth.ts
+++ src/customHooks/useScreenWidth.ts
@@ -3,13 +3,15 @@ import { useState, useEffect } from 'react';
 const useScreenWidth = () => {
   const [width, setWidth] = useState(0);
 
-  const handleResize = () => {
-    setWidth(window.innerWidth);
-  };
-
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    if (typeof window !== 'undefined') {
+      setWidth(window.innerWidth);
+      const handleResize = () => {
+        setWidth(window.innerWidth);
+      };
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }
   }, []);
 
   return width;
