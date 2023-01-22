cfea2d83846f36c9d210887716583eab0e0d029f
ZAPHODh <102484511+ZAPHODh@users.noreply.github.com>
2023-01-22 10:38:59 -0300

    added a schedule icon

--- src/components/Schedule/index.tsx
+++ src/components/Schedule/index.tsx
@@ -1,10 +1,15 @@
 import { Button } from 'components/Button';
 import { useCallback } from 'react';
-
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 export const Schedule = () => {
   const handleClick = useCallback(() => {
     window.location.href =
       'https://api.whatsapp.com/send?phone=5521971083900&text=Olá,%20gostaria%20de%20marcar%20um%20agendamento!%20';
   }, []);
-  return <Button onClick={handleClick}>Agendar!</Button>;
+  return (
+    <Button onClick={handleClick}>
+      Agendar
+      <CalendarMonthIcon fontSize="inherit" />
+    </Button>
+  );
 };
