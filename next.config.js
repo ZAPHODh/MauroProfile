9b63fdd64dfd8b06d6506a6435384c5fcf8d98ad
ZAPHODh <luispaulo.ni@gmail.com>
2023-03-08 07:30:30 -0300

    changes in next configc

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
-
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const dotenv = require('dotenv');
+dotenv.config();
 const nextConfig = {
   reactStrictMode: true,
   images: {
