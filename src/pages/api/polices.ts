d5cfd67d8d6a1eb8bb71e37a315bedf2728a8fa1
ZAPHODh <luispaulo.ni@gmail.com>
2023-03-13 18:26:01 -0300

    fixed all polices issue

--- src/pages/api/polices.ts
+++ src/pages/api/polices.ts
@@ -1,17 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+
 import fs from 'fs';
 import path from 'path';
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const filePath = path.join(
     process.cwd(),
-    '../../../public/assets/files/polices.pdf',
+    'public',
+    'assets',
+    'files',
+    'polices.pdf',
   );
-  const stat = fs.statSync(filePath);
-
-  res.setHeader('Content-Length', stat.size);
+  const fileContents = fs.readFileSync(filePath);
   res.setHeader('Content-Type', 'application/pdf');
-  res.setHeader('Content-Disposition', 'attachment; filename=polices.pdf');
-
-  const stream = fs.createReadStream(filePath);
-  stream.pipe(res);
+  res.send(fileContents);
 }
